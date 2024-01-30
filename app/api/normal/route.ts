7c8a569877caa37761a2edb04ab72d8b212157c5
Andrea Caldera <andrea.caldera@gmail.com>
2024-01-30 21:26:23 +0000

    test timeouts

---
+++ app/api/normal/route.ts
@@ -0,0 +1,15 @@
+import { type NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const delay = searchParams.get("delay")
+    ? Number(searchParams.get("delay"))
+    : 5000;
+  const start = Date.now();
+  await new Promise((resolve) => setTimeout(resolve, delay));
+  const end = Date.now();
+  const duration = (end - start) / 1000;
+  return Response.json({ duration: `${duration} seconds` });
+}
